922092f2a1a270aedf35e41fe5a1d3111fc82a39
iMega34 <mega2207@outlook.com>
2023-11-22 23:11:12 -0600

    S3 Connection class classifies files by the folder they are stored in

--- webApp/other/s3_connection.py
+++ webApp/other/s3_connection.py
@@ -1,6 +1,6 @@
 
 from boto3.session import Session
-import boto3
+from boto3 import client
 
 
 class S3Connection:
@@ -12,43 +12,62 @@ class S3Connection:
     """
 
     def __init__(self) -> None:
-        self._s3_connection: Session = boto3.client(
+        # Connection to the S3 bucket
+        self._s3_connection: Session = client(
             "s3",
             aws_access_key_id="AKIA5RCYKSQAPI3AKL3K",
             aws_secret_access_key="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",
         )
-        self.__files: list[str] = [
-            file["Key"] for file in self._s3_connection.list_objects(Bucket="test-medway-bucket")["Contents"]
+        # Files in the S3 bucket
+        __files = self._s3_connection.list_objects(Bucket="test-medway-bucket")["Contents"]
+        # Daily reports
+        self.__daily_reports: list[str] = [
+            file["Key"].split("/")[-1].split(".")[0]
+            for file in __files
+            if "daily_reports" in file["Key"] and file["Key"] != "daily_reports/"
+        ]
+        # Monthly reports
+        self.__monthly_reports: list[str] = [
+            file["Key"].split("/")[-1].split(".")[0]
+            for file in __files
+            if "monthly_reports" in file["Key"] and file["Key"] != "monthly_reports/"
         ]
 
 
-    def get_file_names(self) -> list[str]:
+    def get_file_names(self, folder: str) -> list[str]:
         """
         Returns the file names from the S3 bucket
 
         Parameters:
-            - No parameters
+            - :param:`folder` (str): The folder in the S3 bucket from where the file names will be returned
 
         Returns:
             - list[str]: The file names from the S3 bucket
         """
 
-        return self.__files
+        if folder == "daily_reports":
+            return self.__daily_reports
+        elif folder == "monthly_reports":
+            return self.__monthly_reports
+        else:
+            return []
 
 
-    def download_file(self, file_name: str) -> None:
+    def download(self, folder: str, download_path: str ,file_name: str) -> None:
         """
         Downloads the file from the S3 bucket
 
         Parameters:
-            - file_name (str): The name of the file to download
+            - :param:`folder` (str): The folder in the S3 bucket where the file is located
+            - :param:`download_path` (str): The path where the file will be downloaded
+            - :param:`file_name` (str): The name of the file to be downloaded
 
         Returns:
             - None
         """
 
         self._s3_connection.download_file(
-            "test-medway-bucket",
-            file_name,
-            f"C:/Users/Dany/Desktop/{file_name}"
+            "test-medway-bucket",               # Bucket name - S3 bucket
+            f"{folder}/{file_name}.pdf",        # Key - Path to the file in the S3 bucket
+            f"{download_path}/{file_name}.pdf"  # Filename - Path where the file will be downloaded
         )
